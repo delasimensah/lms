ef21140cb16d5140d9f98d938168ed73d7fb1a91
delasimensah <mensadelasi@gmail.com>
2023-12-05 10:49:10 +0000

    updated dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,24 +1,26 @@
+import { ReactNode, FC } from "react";
+
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
-const DashboardLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
+type DashboardLayoutProps = {
+  children: ReactNode;
+};
+
+const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
+  return (
     <div className="h-full">
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+      <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
         <Navbar />
       </div>
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+
+      <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
         <Sidebar />
       </div>
-      <main className="md:pl-56 pt-[80px] h-full">
-        {children}
-      </main>
+
+      <main className="h-full pt-[80px] md:pl-56">{children}</main>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DashboardLayout;
