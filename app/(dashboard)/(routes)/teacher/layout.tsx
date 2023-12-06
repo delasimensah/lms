5e3df0f04eb905b2e8692d82c39b47a321446bd3
delasimensah <mensadelasi@gmail.com>
2023-12-06 09:45:10 +0000

    updated teacher route layout

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,19 +1,20 @@
+import { FC, ReactNode } from "react";
 import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const TeacherLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+type TeacherLayoutProps = {
+  children: ReactNode;
+};
+
+const TeacherLayout: FC<TeacherLayoutProps> = ({ children }) => {
   const { userId } = auth();
 
   if (!isTeacher(userId)) {
     return redirect("/");
   }
 
-  return <>{children}</>
-}
- 
+  return <>{children}</>;
+};
+
 export default TeacherLayout;
