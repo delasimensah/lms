df3938f043000b50705f9ef6d9fad8cc73496279
delasimensah <mensadelasi@gmail.com>
2023-12-07 13:36:25 +0000

    updated course layout

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,3 +1,4 @@
+import { FC, ReactNode } from "react";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -7,17 +8,16 @@ import { getProgress } from "@/actions/get-progress";
 import { CourseSidebar } from "./_components/course-sidebar";
 import { CourseNavbar } from "./_components/course-navbar";
 
-const CourseLayout = async ({
-  children,
-  params
-}: {
-  children: React.ReactNode;
+type CourseLayoutProps = {
+  children: ReactNode;
   params: { courseId: string };
-}) => {
+};
+
+const CourseLayout: FC<CourseLayoutProps> = async ({ children, params }) => {
   const { userId } = auth();
 
   if (!userId) {
-    return redirect("/")
+    return redirect("/");
   }
 
   const course = await db.course.findUnique({
@@ -33,12 +33,12 @@ const CourseLayout = async ({
           userProgress: {
             where: {
               userId,
-            }
-          }
+            },
+          },
         },
         orderBy: {
-          position: "asc"
-        }
+          position: "asc",
+        },
       },
     },
   });
@@ -51,23 +51,16 @@ const CourseLayout = async ({
 
   return (
     <div className="h-full">
-      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
-        <CourseNavbar
-          course={course}
-          progressCount={progressCount}
-        />
+      <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-80">
+        <CourseNavbar course={course} progressCount={progressCount} />
       </div>
-      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-        <CourseSidebar
-          course={course}
-          progressCount={progressCount}
-        />
-      </div>
-      <main className="md:pl-80 pt-[80px] h-full">
-        {children}
-      </main>
-    </div>
-  )
-}
 
-export default CourseLayout
+      <div className="fixed inset-y-0 z-50 hidden h-full w-80 flex-col md:flex">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="h-full pt-[80px] md:pl-80">{children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
